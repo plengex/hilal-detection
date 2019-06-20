3de9acbdb404e14d3d59ec20f63affd431c813f0
plengex <plengex09@gmail.com>
2019-06-20 16:41:24 +0700

    menambahkan fungsi pada tombol hitung

--- js/count.js
+++ js/count.js
@@ -12,12 +12,20 @@ hitung.addEventListener("click", ()=>{
         <div class="col-full">
             <h3 style="margin-top: 3rem">Ijtima' Jatuh Pada</h3>
         </div>
+    </div>
+    <div class="row">
         <div class="col-3">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">Hari</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam</p>
         </div>
         <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
         </div>
     </div>
@@ -25,26 +33,76 @@ hitung.addEventListener("click", ()=>{
         <div class="col-full">
             <h3 style="margin-top: 3rem">Kondisi Matahari dan Hilal pada</h3>
         </div>
+    </div>
+    <div class="row">
         <div class="col-3">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">Maghrib</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Matahari Terbenam</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Tinggi Hilal</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Lama Hilal</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Letak Hilal</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Nurul Hilal</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Keadaan Hilal</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Hilal Terbenam</p>
-            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam Hilal Terbenam</p>
         </div>
         <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Matahari Terbenam</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Tinggi Hilal</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Lama Hilal</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Letak Hilal</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Nurul Hilal</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Keadaan Hilal</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Hilal Terbenam</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam Hilal Terbenam</p>
+        </div>
+        <div class="col-8">
             <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
         </div>
     </div>
