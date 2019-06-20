ad328f10e3475d4b4e06c54c19b0a1f4eb83669a
plengex <plengex09@gmail.com>
2019-06-20 16:24:08 +0700

    menambahkan fungsi pada tombol hitung

--- js/count.js
+++ js/count.js
@@ -1,7 +1,54 @@
 let hitung=document.querySelector("#hitung");
 let reset=document.querySelector("#reset");
 hitung.addEventListener("click", ()=>{
-    document.querySelector("#hasil").innerHTML = `<hr><p class="lead" style="color: #000">Sek iki jek gurung dadi</p>`
+    document.querySelector("#hasil").innerHTML = `
+    
+    <div class="row">
+        <div class="col-full">
+            <h3 style="margin-bottom: 0">Hisab Awal Bulan</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-full">
+            <h3 style="margin-top: 3rem">Ijtima' Jatuh Pada</h3>
+        </div>
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Hari</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam</p>
+        </div>
+        <div class="col-8">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-full">
+            <h3 style="margin-top: 3rem">Kondisi Matahari dan Hilal pada</h3>
+        </div>
+        <div class="col-3">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Maghrib</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Matahari Terbenam</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Tinggi Hilal</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Lama Hilal</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Letak Hilal</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Nurul Hilal</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Keadaan Hilal</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Hilal Terbenam</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">Jam Hilal Terbenam</p>
+        </div>
+        <div class="col-8">
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+            <p class="lead" style="color: #000; margin-bottom: 0.5rem">: Sek iki jek gurung dadi</p>
+        </div>
+    </div>
+    `
 });
 
 reset.addEventListener("click", ()=>{
